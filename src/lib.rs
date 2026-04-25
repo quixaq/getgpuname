6ec253937d215d214ea55d693357ec2576d24dbb
Quixaq <quixaq@tutamail.com>
2026-04-25 21:03:01 +0200

    feat!: relicense to GPL-3.0-or-later

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,21 @@
+/*
+ * getgpuname v0.1.5
+ * Copyright (C) 2026  Quixaq
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 //! Gets the name of a GPU from the PCI-IDS database
 mod pci_ids;
 use crate::pci_ids::PCI_IDS;
